54f00c11b85446ae2e882967c93675de094ce3b1
sou <opepelab@gmail.com>
2021-07-13 11:37:25 +0900

    first commit

---
+++ pages/access.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <main>
+          Next.jsとNuxt.jsを<br>
+          直接的なリアル討論は好きではありません。<br>
+          しかしサイヤ人です。<br>
+          <h4>プロフィール</h4>
+          出身住まい：日本 東京<br />
+          生年：1994年生まれ 戌年<br />
+          趣味：読書 <br />
+  </main>
+</template>
+
+<script>
+	export default {
+		head: {
+      title: 'Career - S.watana',
+      meta: [
+        {
+          name:'description',
+          content:'キャリアのページです',
+        }
+      ],
+		}
+	}
+</script>
