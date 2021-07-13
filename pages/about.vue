686dc9081c1b473d3eb97938017e7b940d575087
sou <opepelab@gmail.com>
2021-07-13 12:39:24 +0900

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <h1>Home page</h1>
+    <NuxtLink to="/about">
+      About (internal link that belongs to the Nuxt App)000
+    </NuxtLink>
+    <a href="https://nuxtjs.org">External Link to another page</a>
+  </main>
+</template>
+
+<script>
+	export default {
+		head: {
+      title: 'Career - S.watana',
+      meta: [
+        {
+          name:'description',
+          content:'キャリアのページです',
+        }
+      ],
+		}
+	}
+</script>
