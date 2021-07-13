5bc831fc53367c67fc25bf3cc29696250ada7011
sou <opepelab@gmail.com>
2021-07-13 11:46:20 +0900

    first commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~/styles/globals.css'
+    '@/assets/css/globals.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
