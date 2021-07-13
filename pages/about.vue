04b9fb19788902e917493ba73ccff49ae40edd68
sou <opepelab@gmail.com>
2021-07-13 12:08:57 +0900

    first commit

--- pages/about.vue
+++ pages/about.vue
@@ -2,7 +2,7 @@
   <main>
     <h1>Home page</h1>
     <NuxtLink to="/about">
-      About (internal link that belongs to the Nuxt App)
+      About (internal link that belongs to the Nuxt App)000
     </NuxtLink>
     <a href="https://nuxtjs.org">External Link to another page</a>
   </main>
