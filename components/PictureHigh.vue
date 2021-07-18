d4a35332876190d360f99e6e3f30e500d1f45d4c
sou <opepelab@gmail.com>
2021-07-19 05:58:55 +0900

    07/19

--- components/PictureHigh.vue
+++ components/PictureHigh.vue
@@ -1,13 +1,13 @@
 <template>
         <div class="flex-container">
                     <a class="maru" href="https://github.com/opepelab" target="_blank">
-                        <img class="Maru Toumei ZoomS" src="/images/gitx.png" width="75" height="75" />
+                        <img class="Maru Toumei ZoomS" src="/images/gitx.png" width="70" height="70" />
                     </a>
                     <a class="maru" href="mailto:ocelot2828@gmail.com" target="_blank">
-                        <img class="Maru Toumei ZoomS" src="/images/email.png" width="75" height="75" />
+                        <img class="Maru Toumei ZoomS" src="/images/email.png" width="70" height="70" />
                     </a>
                     <a class="maru" href="https://www.Linkedin.com/in/s-watanabe-a25157205" target="_blank">
-                        <img class="Maru Toumei ZoomS" src="/images/linkedx.png" width="75" height="75" />
+                        <img class="Maru Toumei ZoomS" src="/images/linkedx.png" width="70" height="70" />
                     </a>
         </div>
 </template>
