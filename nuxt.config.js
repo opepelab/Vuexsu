0be1d737f2094dbe941b0465523498467c4a1c33
sou <opepelab@gmail.com>
2021-07-20 20:20:41 +0900

    first commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,6 +8,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'robots', content: 'noindex,nofollow' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
